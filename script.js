699d6fa7907b8f6d466a18189211042e32f05c24
tatilai <99092149+tatilai@users.noreply.github.com>
2024-04-28 18:54:03 -0300

    feat:mostrar pokemones

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url_pokemones= 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
+/*const url_pokemones= 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
 
 //const url_pokemones= 'https://pokeapi.co/api/v2/pokemon';
 
@@ -7,7 +7,14 @@ const mostrarCard = (lista)=>{
     const contenedor=document.getElementById("contenedor");
     const li=document.createElement('li');
     const titulo =document.createElement('h3');
+    titulo.innerText=lista.name;
     li.appendChild(titulo);
+
+
+    const descripcion = document.createElement('p');
+    descripcion.innerText='algo'
+    li.appendChild(descripcion);
+
     contenedor.appendChild(li);
     console.log(contenedor);
 }
@@ -20,4 +27,61 @@ fetch(url_pokemones)
     const results=result.results;
     const primerResultado = results[0];
  mostrarCard(primerResultado)
-})
+})*/
+
+
+const listaPokemon=document.querySelector("#listaPokemon");
+const url_pokemones = "https://pokeapi.co/api/v2/pokemon/";
+
+for (let i=1; i<=100; i++){
+    fetch(url_pokemones + i)
+    .then((response)=> response.json())
+    .then(data=>mostrarPokemon(data))
+}
+
+function mostrarPokemon(data){
+
+    const div =document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML=` 
+    <p class="pokemon-id-back">#025</p>
+     <div class ="pokemon-imagen">
+       <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt= bulbasaur>
+     </div>
+     <div class="pokemon-info">
+       <div class="nombre-contenedor">
+           <p class="pokemon-id">#025</p>
+           <h2 class="pokemon-nombre">${data.name}</h2>
+       </div>
+       <div class="pokemon-tipos">
+        <p class="electric tipo">electric</p>
+         <p class="fighting tipo">fighting</p>
+       </div>
+     <div class="pokemon-stats">
+       <p class="stat">4m</p>
+       <p class ="stat">60kg</p>
+       </div>
+       </div>
+     `;
+     listaPokemon.append(div);
+}
+
+
+/**
+ * <div class="pokemon">
+ * <p class="pokemon-id-back">#025</p>
+ * <div class ="pokemon-imagen">
+ *    <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt= bulbasaur>
+ * </div>
+ * <div class="pokemon-info">
+ *   <div class="nombre-contenedor">
+ *    <p class="electric tipo">electric</p>
+ *     <p class="fighting tipo">fighting</p>
+ *   </div>
+ * <div class="pokemon-stats">
+ *   <p class="stat">4m</p>
+ *   <p class ="stat">60kg</p>
+ * </div>
+ * </div>
+ * </div>  
+ */
