0b8ab58cd0661b1245c6c3aaa2dae2b7682bd070
tatilai <99092149+tatilai@users.noreply.github.com>
2024-04-28 22:28:46 -0300

    cambio estructura

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function mostrarPokemon(data){
     const div =document.createElement("div");
     div.classList.add("pokemon");
     div.innerHTML=` 
-    <p class="pokemon-id-back">${data.id}</p>
+    <p class="pokemon-id-back">#${data.id}</p>
      <div class ="pokemon-imagen">
        <img src= "${data.sprites.other["official-artwork"].front_default}" alt= "${data.name}">
      </div>
@@ -68,22 +68,32 @@ function mostrarPokemon(data){
      listaPokemon.append(div);
 }
 
-
 /**
- * <div class="pokemon">
- * <p class="pokemon-id-back">#025</p>
- * <div class ="pokemon-imagen">
- *    <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt= bulbasaur>
- * </div>
- * <div class="pokemon-info">
- *   <div class="nombre-contenedor">
- *    <p class="electric tipo">electric</p>
- *     <p class="fighting tipo">fighting</p>
- *   </div>
- * <div class="pokemon-stats">
- *   <p class="stat">4m</p>
- *   <p class ="stat">60kg</p>
- * </div>
- * </div>
- * </div>  
+ *  <div class="pokemon">
+
+                <p class="pokemon-id-back">#025</p>
+                <div class ="pokemon-imagen">
+                    <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt= "bulbasaur">
+               </div>
+               <div class="pokemon-info">
+                <div class="nombre-contenedor">
+                    <p class="pokemon-id">
+                        #025</p>
+                        <h2 class="pokemon-nombre">Pikachu</h2>
+                </div>
+                <div class="pokemon-tipos">
+                    <p class="electric tipo">Electric</p>
+                    <p class="fighting tipo">fighting</p>
+                </div>
+
+                <div class="stats">
+                    <p class="stats">4m</p>
+                    <p class="stats">60kg</p>
+                </div>
+
+                <button class="btn btn-mas"> Ver más</button>
+
+               </div>
+
+            </div>
  */
